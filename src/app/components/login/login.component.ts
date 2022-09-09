4e807183010eda31f4c39049af532dffaee36874
aakashvc <aakashvc@infrrd.ai>
2022-09-09 17:58:54 +0530

    added functions to get input from form and credential data

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  username = '';
+  password = '';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onUpdateUsername(event: Event) {
+    this.username = (<HTMLInputElement>event.target).value;
+  }
+
+  onUpdatePassword(event: Event) {
+    this.password = (<HTMLInputElement>event.target).value;
+  }
+
+  loginAuthenticate() {
+    console.log('username: '+this.username);
+    console.log('password: '+this.password)
   }
 
 }
